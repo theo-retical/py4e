ac6f94eb474474f263a2bd784b0a44637e9ea32a
theo <98850030+theo-retical@users.noreply.github.com>
2024-07-03 14:50:25 +0100

    Create ex_GeoJSON_API.py

---
+++ Chapter 13 - Using Web Services/ex_GeoJSON_API.py
@@ -0,0 +1,46 @@
+import urllib.request, urllib.parse
+import json, ssl
+
+# Heavily rate limited proxy of https://www.geoapify.com/ api
+serviceurl = 'https://py4e-data.dr-chuck.net/opengeo?'
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+while True:
+    address = input('Enter location: ')
+    if len(address) < 1: break
+
+    address = address.strip()
+    parms = dict()
+    parms['q'] = address
+
+    url = serviceurl + urllib.parse.urlencode(parms)
+
+    print('Retrieving', url)
+    uh = urllib.request.urlopen(url, context=ctx)
+    data = uh.read().decode()
+       
+    try:
+        js = json.loads(data)
+    
+    except json.decoder.JSONDecodeError:
+        js = None
+
+    if not js or 'features' not in js:
+        print('==== Download error ===')
+        print(data)
+        break
+
+    if len(js['features']) == 0:
+        print('==== Object not found ====')
+        print(data)
+        break
+
+    # obtain plus_code and print
+    plus = js['features'][0]['properties']['plus_code']
+    print("Plus Code", plus)
+
+
